fc851d1f221ecb8d50a81d603f3cf457d2668f67
LucasMondacaDuocUC <lu.mondaca@duocuc.cl>
2022-06-04 15:45:00 -0400

    commit

--- BullTerrier/BullTerrierWeb/urls.py
+++ BullTerrier/BullTerrierWeb/urls.py
@@ -4,13 +4,22 @@ from .views import carrito, comentario, home, pedidos, productocompra, productos
 urlpatterns = [
     #Carrito
     path('carrito', carrito, name="carrito"),
+    #Comentario
     path('comentario', comentario, name="comentario"),
+    #Index
     path('', home, name="index"),
+    #Pedidos
     path('pedidos', pedidos, name="pedidos"),
+    #Producto Compra
     path('productocompra', productocompra, name="productocompra"),
+    #Productos gato
     path('productosgato', productosgato, name="productosgato"),
+    #Productos Hamster
     path('productoshamster', productoshamster, name="productoshamster"),
+    #Productos Perro
     path('productosperro', productosperro, name="productosperro"),
+    #Registro
     path('registro', registro, name="registro"),
+    #Suscribirse
     path('suscribirse', suscribirse, name="suscribirse"),
 ]
