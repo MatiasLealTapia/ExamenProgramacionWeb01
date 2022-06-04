83897810aa604b808cb25ba62351400760e47fb9
LucasMondacaDuocUC <lu.mondaca@duocuc.cl>
2022-06-04 15:43:36 -0400

    commit

--- BullTerrier/BullTerrierWeb/urls.py
+++ BullTerrier/BullTerrierWeb/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
-from .views import home, productosgato, productosperro, productoshamster, productocompra, pedidos, carrito, registro
+from .views import carrito, comentario, home, pedidos, productocompra, productosgato, productoshamster, productosperro, registro, suscribirse
 
 urlpatterns = [
-    path('', home, name="index"),
-    path('productosgato', productosgato, name="productosgato"),
-    path('productosperro', productosperro, name="productosperro"),
-    path('productoshamster', productoshamster, name="productoshamster"),
-    path('productocompra', productocompra, name="productocompra"),
-    path('pedidos', pedidos, name="pedidos"),
+    #Carrito
     path('carrito', carrito, name="carrito"),
+    path('comentario', comentario, name="comentario"),
+    path('', home, name="index"),
+    path('pedidos', pedidos, name="pedidos"),
+    path('productocompra', productocompra, name="productocompra"),
+    path('productosgato', productosgato, name="productosgato"),
+    path('productoshamster', productoshamster, name="productoshamster"),
+    path('productosperro', productosperro, name="productosperro"),
     path('registro', registro, name="registro"),
+    path('suscribirse', suscribirse, name="suscribirse"),
 ]
--- BullTerrier/BullTerrierWeb/views.py
+++ BullTerrier/BullTerrierWeb/views.py
@@ -2,26 +2,42 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
-    return render(request, 'BullTerrierWeb/index.html')
-
-def productosgato(request):
-    return render(request, 'BullTerrierWeb/productosgato.html')
-
-def productosperro(request):
-    return render(request, 'BullTerrierWeb/productosperro.html')
-
-def productoshamster(request):
-    return render(request, 'BullTerrierWeb/productoshamster.html')
-
-def productocompra(request):
-    return render(request, 'BullTerrierWeb/productocompra.html')
-
-def pedidos(request):
-    return render(request, 'BullTerrierWeb/pedidos.html')
-
+# Vista Carrito
 def carrito(request):
     return render(request, 'BullTerrierWeb/carrito.html')
 
+# Vista Comentario
+def carrito(request):
+    return render(request, 'BullTerrierWeb/comentario.html')
+
+# Vista Index
+def home(request):
+    return render(request, 'BullTerrierWeb/index.html')
+
+# Vista Pedidos
+def pedidos(request):
+    return render(request, 'BullTerrierWeb/pedidos.html')
+
+# Vista Producto Compra
+def productocompra(request):
+    return render(request, 'BullTerrierWeb/productocompra.html')
+
+# Vista Productos Gato
+def productosgato(request):
+    return render(request, 'BullTerrierWeb/productosgato.html')
+
+# Vista Productos Hamster
+def productoshamster(request):
+    return render(request, 'BullTerrierWeb/productoshamster.html')
+
+# Vista Productos Perro
+def productosperro(request):
+    return render(request, 'BullTerrierWeb/productosperro.html')
+
+# Vista Registro
 def registro(request):
     return render(request, 'BullTerrierWeb/registro.html')
+
+# Vista Suscribirse
+def suscribirse(request):
+    return render(request, 'BullTerrierWeb/suscribirse.html')
