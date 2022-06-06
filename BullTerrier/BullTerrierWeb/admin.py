27533666d8ddd3caacceceec575af08a63c1935b
LucasMondacaDuocUC <lu.mondaca@duocuc.cl>
2022-06-06 01:52:12 -0400

    Merge branch 'lucas-branch'

--- BullTerrier/BullTerrierWeb/admin.py
+++ BullTerrier/BullTerrierWeb/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Usuario
+from .models import Categoria, Compra, Usuario, Producto, Pedido
 
 # Register your models here.
 
 admin.site.register(Usuario)
+admin.site.register(Producto)
+admin.site.register(Compra)
+admin.site.register(Pedido)
+admin.site.register(Categoria)
